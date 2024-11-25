119be95e1c92c154467e833e7718a60c9c898d6b
Manuel Alejandro Peregrino Clemente <manuperegrino1@gmail.com>
2024-11-25 04:06:36 -0600

    reworked in order to test a local API for communication

--- octoprint_automaker/static/js/automaker.js
+++ octoprint_automaker/static/js/automaker.js
@@ -1,68 +1,21 @@
-/*
- * View model for OctoPrint-Automaker
- *
- * Author: Manuel Alejandro Peregrino Clemente
- * License: AGPLv3
- */
-$(function () {
+$(function() {
     function AutomakerViewModel(parameters) {
         var self = this;
 
-        self.username = ko.observable("");
-        self.password = ko.observable("");
-        self.isLoggedIn = ko.observable(false);
-        self.settings = parameters[0];
-
-        self.login = function () {
-            if (self.username() && self.password()) {
-                $.ajax({
-                    url: API_BASEURL + "plugin/automaker/login",
-                    type: "POST",
-                    dataType: "json",
-                    data: JSON.stringify({
-                        command: "login",
-                        username: self.username(),
-                        password: self.password()
-                    }),
-                    contentType: "application/json; charset=UTF-8",
-                    success: function (response) {
-                        if (response.status === "success") {
-                            self.isLoggedIn(true);
-                            alert("Login successful!");
-                        } else {
-                            alert("Login failed: " + response.message);
-                        }
-                    },
-                    error: function () {
-                        alert("An error occurred during login.");
-                    }
-                });
-            } else {
-                alert("Please enter a username and password.");
-            }
-        };
-
-        self.logout = function () {
+        self.monitorPrinter = function() {
             $.ajax({
-                url: API_BASEURL + "plugin/automaker",
+                url: "http://localhost:8000/default/monitor_printer_monitor__post",
                 type: "POST",
                 dataType: "json",
                 data: JSON.stringify({
-                    command: "logout"
+                    monitoring: true
                 }),
                 contentType: "application/json; charset=UTF-8",
-                success: function (response) {
-                    if (response.status === "success") {
-                        self.isLoggedIn(false);
-                        self.username("");
-                        self.password("");
-                        alert("Logout successful");
-                    } else {
-                        alert("Logout failed: " + response.message);
-                    }
+                success: function(response) {
+                    alert("Monitoring started successfully!");
                 },
-                error: function () {
-                    alert("An error occurred during logout.");
+                error: function() {
+                    alert("An error occurred while starting monitoring.");
                 }
             });
         };
